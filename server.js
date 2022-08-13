497532331c8be1a5d81ff59b0a54836cd7219d0e
Chelsea Liddy <107088670+chelsea-liddy@users.noreply.github.com>
2022-08-13 15:33:16 +1200

    write function to read data.json

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const server = express()
 const hbs = require('express-handlebars')
+const fs = require('fs')
 
 server.use(express.static('public'))
 server.use(express.urlencoded({ extended: false }))
 
 server.engine('hbs', hbs.engine({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
+
+function readData(callback) {
+  fs.readFile('./data.json', 'utf-8', (err, data) => {
+    const viewData = JSON.parse(data)
+    if (err) {
+      console.log('Whoops, couldn`t read that!')
+    } else {
+      callback(viewData)
+    }
+  })
+}
 
 server.get('/', (req, res) => {
   res.render('home')
