69f6361a0aea07a5e3c8fcc53c8724a671c97b43
Chelsea Liddy <107088670+chelsea-liddy@users.noreply.github.com>
2022-08-13 16:01:52 +1200

    replace with server test

--- index.test.js
+++
@@ -1,8 +0,0 @@
-const index = require('./index')
-
-describe('sayHello', () => {
-  test('greets by name', () => {
-    expect(index.sayHello('Chelsea')).toEqual('Hello Chelsea!')
-    expect(index.sayHello('Lani')).toEqual('Hello Lani!')
-  })
-})
