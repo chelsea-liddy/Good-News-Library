c6a498a2bcb1612667123c171fbd8c1efd77fb7d
Chelsea Liddy <107088670+chelsea-liddy@users.noreply.github.com>
2022-08-13 15:53:36 +1200

    write practice test

--- server.test.js
+++ server.test.js
@@ -1,1 +1,17 @@
+const request = require('supertest')
+const { screen } = require('@testing-library/dom')
+require('@testing-library/jest-dom')
 
+const server = require('./server')
+
+describe('GET /', () => {
+  test('display heading', () => {
+    return request(server)
+      .get('/')
+      .then((res) => {
+        document.body.innerHTML = res.text
+        const headings = screen.getAllByRole('heading')
+        expect(headings[[1]]).toHaveTextContent('Welcome')
+      })
+  })
+})
